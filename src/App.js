f2101d9c3086cefba45a2c4f58c92b9d32573e10
valecurcio <vale.s.curcio@gmail.com>
2021-09-08 18:07:26 -0300

    Clase 2 - Instalación y configuración del entorno

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Clase 2 React - Coder House
         </p>
         <a
           className="App-link"
